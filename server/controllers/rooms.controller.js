28c85ededce79c54d414669e1a8e7babf526ca85
vvalll <valeng22092@gmail.com>
2023-09-17 22:38:31 -0500

    cambiosV3

--- server/controllers/rooms.controller.js
+++ server/controllers/rooms.controller.js
@@ -26,13 +26,11 @@ export const createRoom = async (req, res) => {
 
     try {
         const [rows] = await pool.query('INSERT INTO `habitaciones` (`numero_habitacion`, `imagen`, `tipo_habitacion_id`, `id_estado`) VALUES(?,?,?,?)', [numero_habitacion, imagen, tipo_habitacion_id, id_estado])
-        res.send({
-            habitacion_id: rows.insertId,
-            numero_habitacion,
-            imagen,
-            tipo_habitacion_id,
-            id_estado
-        })
+
+        const [rows2] = await pool.query('SELECT habitaciones.habitacion_Id, habitaciones.numero_habitacion, habitaciones.imagen, tipo_habitaciones.nombre_tipo_habitacion, tipo_habitaciones.descripcion, tipo_habitaciones.precio_base, tipo_habitaciones.capacidad, estado_habitacion.nombre FROM habitaciones INNER JOIN tipo_habitaciones ON habitaciones.tipo_habitacion_id = tipo_habitaciones.tipo_habitacion_id INNER JOIN estado_habitacion ON habitaciones.id_estado = estado_habitacion.id_estado WHERE habitacion_id = ?', [rows.insertId])
+
+        res.json(rows2[0])
+
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
@@ -47,7 +45,7 @@ export const updateRoom = async (req, res) => {
 
         if(result.affectedRows === 0) return res.status(404).json({message: `No se pudo actualizar ninguna habitacion por el id: ${req.params.id}`})
 
-        const [rows] = await pool.query('SELECT * FROM `habitaciones` WHERE habitacion_id = ?', [id])
+        const [rows] = await pool.query('SELECT habitaciones.habitacion_Id, habitaciones.numero_habitacion, habitaciones.imagen, tipo_habitaciones.nombre_tipo_habitacion, tipo_habitaciones.descripcion, tipo_habitaciones.precio_base, tipo_habitaciones.capacidad, estado_habitacion.nombre FROM habitaciones INNER JOIN tipo_habitaciones ON habitaciones.tipo_habitacion_id = tipo_habitaciones.tipo_habitacion_id INNER JOIN estado_habitacion ON habitaciones.id_estado = estado_habitacion.id_estado WHERE habitacion_id = ?', [id])
 
         res.json(rows[0])
     }   catch (error) {
