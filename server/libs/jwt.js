0cd038a56f250a2dccedb8f625e22f749c949b16
NICOLAS-1011 <nicolasandresespitiavillamil@gmail.com>
2023-12-05 21:21:19 -0500

    Document (auth, jwt, vTok)

--- server/libs/jwt.js
+++ server/libs/jwt.js
@@ -1,5 +1,11 @@
+//Importa una clave del archivo config.js
 import { SECRETKEY } from "../config.js";
+
+//Importa la funcion que va a crear token, en base de archivos json
 import jwt from "jsonwebtoken";
+
+
+//Metodo para crear el token que solicita un json como parametro, genera una promesa que tiene dos tipos de respuesta, añadiendole a la funcion jwt el "sign", este crea con los campos de el token que se le paso, la contrasena del json, y en caso de hayar un error elegira la opcion de reject, si no devovera el tocken construido.
 
 export function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,12 @@
+//Trae una funcion de express para establecer rutas
 import { Router } from "express";
 
-
+//crea la constante de la funcion
 const router = Router();
 
+//Crea un metodo que valide si ya existe una cookie con ese usuario o si debe volver a crearla como un usuario nuevo, retornara un error si no encuentra la cookie de usuario ????
+
+//Realiza una solicitud que desea obtener True si puede obtener respuesta del usuario, si no retornara un error que informa que el recurso no fue encontrado
 router.get("/verify", (req, res) => {
   if (req.user) {
     res.send(true);
